8adf60647f4cc27af12cd9be60802d53ec85a1b6
gor98 <azizyan.gor.98@gmail.com>
2020-08-24 15:00:19 +0400

    response object and remove unnesseccery files

---
+++ app/Common/Tools/APIResponse.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Common\Tools;
+
+use Illuminate\Http\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Class APIResponse
+ *
+ * @package App\Common\Tools
+ */
+class APIResponse
+{
+    /**
+     * @param array $responseObject
+     * @param int $responseCode
+     * @param array $headers
+     * @return JsonResponse
+     */
+    public static function successResponse(
+        array $responseObject,
+        int $responseCode = Response::HTTP_OK,
+        array $headers = []
+    ): JsonResponse {
+        return response()->json(['data' => $responseObject], $responseCode, $headers);
+    }
+
+    /**
+     * @param array $responseObject
+     * @param int $responseCode
+     * @param array $headers
+     * @return JsonResponse
+     */
+    public static function errorResponse(
+        array $responseObject,
+        int $responseCode = Response::HTTP_BAD_REQUEST,
+        array $headers = []
+    ): JsonResponse {
+        return response()->json(['error' => $responseObject], $responseCode, $headers);
+    }
+}
